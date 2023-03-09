4678ed209d163c184ec0f3bf27ad0f22747d7e78
Camila Fernandes <108847180+CamilaFernandesdev@users.noreply.github.com>
2023-03-08 22:29:53 -0300

    Update oop_script_test.py

--- oop_script_test.py
+++ oop_script_test.py
@@ -1,4 +1,3 @@
-
 from pathlib import Path
 import pandas as pd
 
@@ -7,7 +6,9 @@ import pandas as pd
 
 class ConsumoCCEE():
     def __init__(self, _filepath: Path) -> None:
-        self.codigo_perfil = None
+        
+        # Informações da base de dados
+        self.codigo_perfil = set()
         self.classe_perfil_do_agente = None
         self.sigla = None
         self.nome_empresarial = None
@@ -31,43 +32,72 @@ class ConsumoCCEE():
         self.consumo_3 = None
         self.consumo_4 = None
         
-        
+        # Documentos para o Mongo
+        self.infomercado_ccee_empresa = list()
+        self.infomercado_ccee_consumo_carga = list() #lista?
+        self.infomercado_ccee_empresa_unidades = list()
+
+        # Apenas constantes
+        CHUNK_SIZE = 10**5
         for i, chunk in enumerate(pd.read_csv(_filepath,
-                                              chunksize=100000,
+                                              chunksize=CHUNK_SIZE,
                                               sep=';',
                                               decimal=',')):
+          
+          
+          for index, rows in chunk:
+              self.empresas(row)
+
+
                                          
             pass
     
     
-    def informacoes(self, chunk):
+    def informacoes_chunks(self, i:int, chunk:pd.DataFrame):
         self.old_names_columns = list(chunk.columns)
         
     
-    def empresas(chunk: pd.DataFrame) -> pd.DataFrame:
-        set_empresas = set()
-        
-        
-    def unidades_carga(chunk: pd.DataFrame) -> pd.DataFrame:
-        set_unidades_carga = set()
-        
-        
+    def empresas(rows: pd.Series) -> list[dict]:
+        # Definindo cada empresa de acordo com o código do perfil
+        codigo_perfil = rows[self.old_names_columns[2]]
+
+        # Retirando as redundâncias
+        self.codigo_perfil.add(codigo_perfil)
+
+        # Informações para o Mongo na Colletion Empresa
+        if codigo_perfil not in self.codigo_perfil:
+            self.infomercado_ccee_empresa.append({
             
-        pass    
+            'codigo_perfil': rows[self.old_names_columns[2]],
+            'sigla': rows[self.old_names_columns[3]],
+            'classe_perfil_do_agente': rows[self.old_names_columns[4]],
+            'nome_empresarial': rows[self.old_names_columns[5]],
+            'ramo_de_atividade': rows[self.old_names_columns[20]],
+        
+            })
+        
+    def unidades_carga(rows: pd.Series) -> list[list[dict]]:
+        set_unidades_carga = set()
+        codigo_carga = rows[OLD_NAMES[6]]
+        
+        
+        
+        if codigo_carga not in set_codigo_carga:
+            consumo_unidade_carga.append({
+                
+                'codigo_unidade_carga': rows[OLD_NAMES[6]],
+                'nome_unidade_carga': rows[OLD_NAMES[7]],
+                'cnpj_carga': rows[OLD_NAMES[8]],
+                'cidade': rows[OLD_NAMES[9]],
+                'unidade_federativa': rows[OLD_NAMES[10]],
+                'submercado': rows[OLD_NAMES[11]].strip(),
+                'data_de_migracao': rows[OLD_NAMES[12]],
+                'cogigo_perfil_distribuidora': rows[OLD_NAMES[13]],
+                'sigla_perfil_distribuidora': rows[OLD_NAMES[14]],
+                'capacidade_da_carga(MW)': rows[OLD_NAMES[15]],
+            
+            })
 
-
-class Empresas():
-    def __init__(self):
-        self.codigo_perfil = None
-        self.classe_perfil_do_agente = None
-        self.sigla = None
-        self.nome_empresarial = None
-        self.ramo_atividade = None
-    pass
-
-
-class Datas():
-    pass
-
-class UnidadeEmpresas():
-    pass
+        
+    def carregar_mongo():     
+        pass   
