c0b296bc1b0469a9e4bba6ba56e4607289e2d585
Camila Fernandes <108847180+CamilaFernandesdev@users.noreply.github.com>
2023-03-13 19:02:11 -0300

    Update estrutura_code.py

--- estrutura_code.py
+++ estrutura_code.py
@@ -1,18 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  9 17:41:21 2023
+
+@author: E805511
+"""
+
+
+
 import pandas as pd
 from pathlib import Path
+from datetime import datetime
 
 _filepath = Path("C:/Users/E805511/Downloads/Consumo_horario_2022_12/Consumo_horario_2022_12.csv")
 
 # lê o arquivo csv em chunks de 1000 linhas
-chunk_size = 10000
+chunk_size = 1000
 
 # informações
 
 self_codigo_perfil = set()
+self_codigo_carga = set()
 empresas = list()
 infomercado_ccee_empresa = list()
-
-
+infomercado_ccee_consumo_unidade = list()
+dict_empresa_unidade = dict()
 
 
 
@@ -24,15 +35,15 @@ for i, chunk in enumerate(pd.read_csv(_filepath,
                                       low_memory=False,
                                       decimal = ',')):
     
-    if (i == 4):
+    if (i == 1):
         break
     # Informações
     
     
     last_hour = chunk.iloc[-1]["HH"]
     old_names_columns = list(chunk.columns)
-    dict_empresa_unidade = chunk.groupby('Cód. Perfil').agg({'Cód. Carga':lambda x: list(set(x))}).to_dict(orient='index')
-    
+    dict_empresa_unidade.update(chunk.groupby('Cód. Perfil').agg({'Cód. Carga':lambda x: list(set(x))}).to_dict(orient='index'))
+    empresas
     
     # Estrutura
     for index, rows in chunk.iterrows(): 
@@ -41,7 +52,7 @@ for i, chunk in enumerate(pd.read_csv(_filepath,
         # DADOS EMPRESA
         #==========================================================================
         codigo_perfil = rows[old_names_columns[2]]
-
+        codigo_carga = rows[old_names_columns[6]]
         # Informações para o Mongo na Colletion Empresa
         if codigo_perfil not in self_codigo_perfil:
             infomercado_ccee_empresa.append({
@@ -51,28 +62,111 @@ for i, chunk in enumerate(pd.read_csv(_filepath,
             'classe_perfil_do_agente': rows[old_names_columns[4]],
             'nome_empresarial':        rows[old_names_columns[5]],
             'ramo_de_atividade':       rows[old_names_columns[20]],
-        
+            'unidade':                 list()
             })
             
-        # Retirando as redundâncias
-        self_codigo_perfil.add(codigo_perfil)
+            # Retirando as redundâncias
+            self_codigo_perfil.add(codigo_perfil)
+            
+            
+            if codigo_carga not in self_codigo_carga: 
+            
+            # Constração do dict e tratamento dos dados
+                infomercado_ccee_consumo_unidade.append({
+                    
+                    'codigo_unidade_carga':        rows[old_names_columns[6]],
+                    'nome_unidade_carga':          rows[old_names_columns[7]],
+                    'cnpj_carga':                  str(rows[old_names_columns[8]]).replace('.',''),
+                    'cidade':                      rows[old_names_columns[9]],
+                    'unidade_federativa':          rows[old_names_columns[10]].strip(),
+                    'submercado':                  rows[old_names_columns[11]].strip(),
+                    'data_de_migracao':            rows[old_names_columns[12]],
+                    'cogigo_perfil_distribuidora': rows[old_names_columns[13]],
+                    'sigla_perfil_distribuidora':  rows[old_names_columns[14]],
+                    'capacidade_da_carga(MW)':     rows[old_names_columns[15]],
+                
+                })
+                
+                self_codigo_carga.add(codigo_carga)
         
         
+                # Adicionando as unidades à lista de unidades da empresa correspondente
+        for empresa in infomercado_ccee_empresa:
+            perfil = empresa['codigo_perfil']
+            unidades = dict_empresa_unidade[codigo_perfil]['Cód. Carga']
+            for unidade in unidades:
+                
+                if codigo_carga not in self_codigo_carga:
+                    empresa['unidade'].append({
+                        
+                        'codigo_unidade_carga':        rows[old_names_columns[6]],
+                        'nome_unidade_carga':          rows[old_names_columns[7]],
+                        'cnpj_carga':                  str(rows[old_names_columns[8]]).replace('.',''),
+                        'cidade':                      rows[old_names_columns[9]],
+                        'unidade_federativa':          rows[old_names_columns[10]].strip(),
+                        'submercado':                  rows[old_names_columns[11]].strip(),
+                        'data_de_migracao':            rows[old_names_columns[12]],
+                        'cogigo_perfil_distribuidora': rows[old_names_columns[13]],
+                        'sigla_perfil_distribuidora':  rows[old_names_columns[14]],
+                        'capacidade_da_carga(MW)':     rows[old_names_columns[15]],
+                    
+                    })
+                    
+                    self_codigo_carga.add(codigo_carga)
+                    break
+
         #==========================================================================
         # DADOS UNIDADES (CARGA)
         #==========================================================================
-        # Constração do dict e tratamento dos dados
-        # consumo_unidade_carga.append({
+        # for dicionarios_empresas in infomercado_ccee_empresa: 
+
+        #     if codigo_carga not in self_codigo_carga: 
             
-        #     'codigo_unidade_carga': rows[OLD_NAMES[6]],
-        #     'nome_unidade_carga': rows[OLD_NAMES[7]],
-        #     'cnpj_carga': str(rows[OLD_NAMES[8]]).replace('.',''),
-        #     'cidade': rows[OLD_NAMES[9]],
-        #     'unidade_federativa': rows[OLD_NAMES[10]],
-        #     'submercado': rows[OLD_NAMES[11]].strip(),
-        #     'data_de_migracao': rows[OLD_NAMES[12]],
-        #     'cogigo_perfil_distribuidora': rows[OLD_NAMES[13]],
-        #     'sigla_perfil_distribuidora': rows[OLD_NAMES[14]],
-        #     'capacidade_da_carga(MW)': rows[OLD_NAMES[15]],
+        #     # Constração do dict e tratamento dos dados
+        #         infomercado_ccee_consumo_unidade.append({
+                    
+        #             'codigo_unidade_carga':        rows[old_names_columns[6]],
+        #             'nome_unidade_carga':          rows[old_names_columns[7]],
+        #             'cnpj_carga':                  str(rows[old_names_columns[8]]).replace('.',''),
+        #             'cidade':                      rows[old_names_columns[9]],
+        #             'unidade_federativa':          rows[old_names_columns[10]].strip(),
+        #             'submercado':                  rows[old_names_columns[11]].strip(),
+        #             'data_de_migracao':            rows[old_names_columns[12]],
+        #             'cogigo_perfil_distribuidora': rows[old_names_columns[13]],
+        #             'sigla_perfil_distribuidora':  rows[old_names_columns[14]],
+        #             'capacidade_da_carga(MW)':     rows[old_names_columns[15]],
+                
+        #         })
         
-        # }
+        
+        
+        # # Adicionando as unidades à lista de unidades da empresa correspondente
+        # for empresa in infomercado_ccee_empresa:
+        #     codigo_perfil = empresa['codigo_perfil']
+        #     unidades = dict_empresa_unidade[codigo_perfil]['Cód. Carga']
+        #     for unidade in unidades:
+        #         for unidade_dict in infomercado_ccee_consumo_unidade:
+        #             if unidade_dict['codigo_unidade_carga'] == unidade:
+        #                 empresa['unidade'].append(unidade_dict)
+        #                 break
+
+        # Retirando as redundâncias
+        self_codigo_carga.add(codigo_carga)
+        
+        
+        
+        
+        #==============
+        # DADOOS
+        
+        # dados = {
+            
+        #     'mes': ,
+        #     'horario': ,
+        #     'data_de_migracao': ,
+        # 'unidade'
+        # '
+            
+            
+            
+        #     }
